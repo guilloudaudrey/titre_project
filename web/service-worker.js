db4a12ffad9e8ab3d2f5b5ad645c196f4b3b710e
audrey <audrey.guilloud2201@gmail.com>
2018-09-30 15:02:00 +0200

    logo

--- web/service-worker.js
+++ web/service-worker.js
@@ -91,14 +91,10 @@ self.addEventListener('fetch', function(event) {
 
 **/
 
-var cacheName = 'blog-1';
-var dataCacheName = 'data-blog-1';
+var cacheName = 'typo';
+var dataCacheName = 'data-typo';
 var filesToCache = [
     './app.php',
-
-
-
-
 ];
 
 self.addEventListener('install', function(e) {
@@ -130,13 +126,6 @@ self.addEventListener('fetch', function(e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
     var dataUrl = '/api';
     if (e.request.url.indexOf(dataUrl) > -1) {
-        /*
-         * When the request URL contains dataUrl, the app is asking for fresh
-         * weather data. In this case, the service worker always goes to the
-         * network and then caches the response. This is called the "Cache then
-         * network" strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-         */
         e.respondWith(
             caches.open(dataCacheName).then(function(cache) {
                 return fetch(e.request).then(function(response){
@@ -146,11 +135,6 @@ self.addEventListener('fetch', function(e) {
             })
         );
     } else {
-        /*
-         * The app is asking for app shell files. In this scenario the app uses the
-         * "Cache, falling back to the network" offline strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-         */
         e.respondWith(
             caches.match(e.request).then(function(response) {
                 return response || fetch(e.request);
