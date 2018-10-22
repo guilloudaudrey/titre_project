1b2a09bff7f05615d1fbca22487986f7ab5ed0b5
audrey <audrey.guilloud2201@gmail.com>
2018-10-22 13:52:10 +0200

    api

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,4 +1,4 @@
-/**function activatePlacesSearch() {
+function activatePlacesSearch() {
     var input = document.getElementById('city_input');
     var autocomplete = new google.maps.places.Autocomplete(input)
-}**/
+}
